b777c1c0feedd97279d0a4ab16ee2758f014ae00
Gregory R. Everitt <gregory@gregserv>
2012-07-10 01:13:42 -0400

    Fixed bug where phone numbers were processed as small-ish INTs and those with high values would not be stored.

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS OPUS;
 USE OPUS;
 CREATE TABLE employees (id INT(30), 
-    first_name VARCHAR(10), 
-    last_name VARCHAR(10), 
-    phone_number INT(10), 
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30), 
+    phone_number CHAR(10), 
     location ENUM("New Jersey", "New York", "California"), 
     PRIMARY KEY(id));
