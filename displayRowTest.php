ba111a898de84660aae4d9138056a4efe2168dd2
Gregory R. Everitt <gregory@gregserv>
2012-07-24 03:56:26 -0400

    Added sample page to display table, along with CSS file to set the table's appearance.

---
+++ displayRowTest.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="content-type">
+<title>display row test</title>
+<link rel="stylesheet" href="table.css" type="text/css">
+</head>
+<body>
+<?php
+require 'dbFunctions.php';
+
+displayTable();
+
+?>
+
+  <p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a>
+  </p>
+
+</body>
+</html>
