dfa0408fbb25f8aa3617a43dd91f83d27ce77af8
Gregory R. Everitt <gregory@gregserv>
2012-08-07 00:51:54 -0400

    footer now uses local images for HTML & CSS verification buttons, allowing for secure loading of images over HTTPS

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <p>
     <a href="http://validator.w3.org/check?uri=referer"><img
-      src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a>
+      src="valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a>
 	<a href="http://jigsaw.w3.org/css-validator/check/referer">
         <img style="border:0;width:88px;height:31px"
-            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            src="vcss"
             alt="Valid CSS!">
     </a>
 </p>
