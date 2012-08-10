c8a60927863cd92746d9b94b16fd2b0b9d43422a
Gregory R. Everitt <gregory@gregserv>
2012-08-10 00:51:32 -0400

    changed doctype to HTML5 in add.php and made tags self-closing

--- add.php
+++ add.php
@@ -1,33 +1,33 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
 
 <head>
 <title>Add record</title>
-<meta content="text/html; charset=utf-8" http-equiv="content-type">
-<link rel="stylesheet" href="table.css" type="text/css">
+<meta content="text/html; charset=utf-8" http-equiv="content-type" />
+<link rel="stylesheet" href="table.css" type="text/css" />
 </head>
 
 <body>
 <?php
 include 'header.php';
 ?>
-<form action="addAndConfirm.php" method="post">
+<form action="addAndConfirm.php" method="post" />
 
 <p>
-Enter the employee ID: <input type="text" name="id" size="30"> <br>
-Enter the first name: <input type="text" name="fname" size="10"> <br>
-Enter the last name: <input type="text" name="lname" size="10"> <br>
-Enter the phone number (format as XXXXXXXXXX): <input type="text" name="phone" size="10"> <br>
+Enter the employee ID: <input type="text" name="id" size="30" /> <br />
+Enter the first name: <input type="text" name="fname" size="10" /> <br />
+Enter the last name: <input type="text" name="lname" size="10" /> <br />
+Enter the phone number (format as XXXXXXXXXX): <input type="text" name="phone" size="10" /> <br />
 Enter the location:
 <select name="location">
     <option value="">Select location, please.</option>
     <option value="New Jersey">New Jersey</option>
     <option value="New York">New York</option>
     <option value="California">California</option>
-</select> <br>
+</select> <br />
 </p>
 
-<p><input type="submit" value="Send"></p>
+<p><input type="submit" value="Send" /></p>
 
 </form>
 
