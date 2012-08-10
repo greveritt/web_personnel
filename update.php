b845674697a270808302a06e07c3c7b50432860a
Gregory R. Everitt <gregory@gregserv>
2012-08-10 01:12:49 -0400

    changed doctype for update.php and made tags self-closing

--- update.php
+++ update.php
@@ -1,33 +1,33 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
 
 <head>
 <title>Update record</title>
-<meta content="text/html; charset=utf-8" http-equiv="content-type">
-<link rel="stylesheet" href="table.css" type="text/css">
+<meta content="text/html; charset=utf-8" http-equiv="content-type" />
+<link rel="stylesheet" href="table.css" type="text/css" />
 </head>
 
 <body>
 <?php
 include 'header.php';
 ?>
-<form action="updateAndConfirm.php" method="post">
+<form action="updateAndConfirm.php" method="post" />
 
 <p>
-Enter the ID of the record you want to update: <input type="text" name="id" size="30"> <br>
-Enter the new first name: <input type="text" name="fname" size="10"> <br>
-Enter the new last name: <input type="text" name="lname" size="10"> <br>
-Enter the new phone number (format as XXXXXXXXXX): <input type="text" name="phone" size="10"> <br>
+Enter the ID of the record you want to update: <input type="text" name="id" size="30" /> <br />
+Enter the new first name: <input type="text" name="fname" size="10" /> <br />
+Enter the new last name: <input type="text" name="lname" size="10" /> <br />
+Enter the new phone number (format as XXXXXXXXXX): <input type="text" name="phone" size="10" /> <br />
 Enter the new location:
 <select name="location">
     <option value="">Select location, please.</option>
     <option value="New Jersey">New Jersey</option>
     <option value="New York">New York</option>
     <option value="California">California</option>
-</select> <br>
+</select> <br />
 </p>
 
-<p><input type="submit" value="Send"></p>
+<p><input type="submit" value="Send" /></p>
 
 </form>
 
