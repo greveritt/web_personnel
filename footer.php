b4de0e6e72caf46fc62748db1ba52da66d9aa479
Gregory R. Everitt <gregory@gregserv>
2012-08-10 00:45:51 -0400

    changed footer so that image for the HTML verifier button says 'HTML5'
    actually remembered to save footer.php so that image for the HTML verifier button says 'HTML5'

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <p>
     <a href="http://validator.w3.org/check?uri=referer"><img
-      src="valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a>
+      src="w3cvalidhtml5.png" alt="Valid HTML5!" height="31" width="88"></a>
 	<a href="http://jigsaw.w3.org/css-validator/check/referer">
         <img style="border:0;width:88px;height:31px"
             src="vcss"
