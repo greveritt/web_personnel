909b133b88f5f31a457bf4c6db901726906f721d
Gregory R. Everitt <gregory@gregserv>
2012-08-10 01:08:19 -0400

    changed doctype for deleteAndConfirm.php and made tags self-closing

--- deleteAndConfirm.php
+++ deleteAndConfirm.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
 
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="content-type">
-<link rel="stylesheet" href="table.css" type="text/css">
+<meta content="text/html; charset=utf-8" http-equiv="content-type" />
+<link rel="stylesheet" href="table.css" type="text/css" />
 <title>output</title>
 </head>
 
