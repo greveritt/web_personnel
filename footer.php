ecefab8ca174243d97a63793a866353deb48bd1c
Gregory R. Everitt <gregory@gregserv>
2012-08-10 01:09:45 -0400

    footer now has self-closing tags

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <p>
     <a href="http://validator.w3.org/check?uri=referer"><img
-      src="w3cvalidhtml5.png" alt="Valid HTML5!" height="31" width="88"></a>
+      src="w3cvalidhtml5.png" alt="Valid HTML5!" height="31" width="88" /></a>
 	<a href="http://jigsaw.w3.org/css-validator/check/referer">
         <img style="border:0;width:88px;height:31px"
             src="vcss"
-            alt="Valid CSS!">
+            alt="Valid CSS!" />
     </a>
 </p>
-<hr>
+<hr />
 <p>
 <span style="font-family: sarif;">web_personnel Copyright &copy; Gregory Everitt 2012</span>
 </p>
