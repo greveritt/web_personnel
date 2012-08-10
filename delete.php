804bd9bdecbc75518d695e4496382acdd96ed304
Gregory R. Everitt <gregory@gregserv>
2012-08-10 01:06:33 -0400

    changed doctype for delete.php and made tags self-closing

--- delete.php
+++ delete.php
@@ -1,23 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
 
 <head>
 <title>Delete record</title>
-<meta content="text/html; charset=utf-8" http-equiv="content-type">
-<link rel="stylesheet" href="table.css" type="text/css">
+<meta content="text/html; charset=utf-8" http-equiv="content-type" />
+<link rel="stylesheet" href="table.css" type="text/css" />
 </head>
 
 <body>
 <?php
 include 'header.php';
 ?>
-<form action="deleteAndConfirm.php" method="post">
+<form action="deleteAndConfirm.php" method="post" />
 
 <p>
-Enter the ID of the record you want to delete: <input type="text" name="id" size="30"> 
+Enter the ID of the record you want to delete: <input type="text" name="id" size="30" /> 
 </p>
 
-<p><input type="submit" value="Send"></p>
+<p><input type="submit" value="Send" /></p>
 
 </form>
 
